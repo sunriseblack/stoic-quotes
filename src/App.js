e8ee909d4f3573adc5213c720c954f5f1d16ab7f
Kotenko Anton <anton@octobus.io>
2021-03-08 19:56:19 +0200

    pull

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+window.React = React
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class StoicQuote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      author: ''
+    };
+    this.submitQuote = this.submitQuote.bind(this);
+  }
+
+  submitQuote() {
+    fetch('https://stoic-server.herokuapp.com/random')
+        .then(response => response.json())
+        .then(data => this.setState({text: data[0].body, author: data[0].author}))
+  }
+
+  componentDidMount() {
+    this.submitQuote()
+  }
+
+  render() {
+    return (
+        <div className='wrapper'>
+          <div id='quote-box'>
+            <div id='text'><span id='quote-mark'>"</span>{this.state.text}</div>
+            <div id='author'>– {this.state.author}</div>
+            <a id='tweet-quote'></a><button id='new-quote' onClick={this.submitQuote}>New Quote</button>
+          </div>
+        </div>
+
+    )
+  }
 }
 
-export default App;
+export default StoicQuote
